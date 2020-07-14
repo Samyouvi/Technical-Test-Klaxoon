8ca899824423983f12fceebe0b701d328f06521a
Maël <mael.ortiz@gmail.com>
2020-07-14 12:51:09 +0200

    update

--- project/src/components/Bookmarks.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Bookmarks from './Bookmarks';
-
-test('Bookmarks renders successfully', () => {
-  render(<Bookmarks />);
-});
